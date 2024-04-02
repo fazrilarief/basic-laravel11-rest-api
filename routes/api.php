08951f1336c6ccccfedcc68f9dcccf9e3d15a280
Fazril Arief Nugraha <fazrilarief331@gmail.com>
2024-04-02 11:52:20 +0700

    change routing to apiResource

--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{id}', [BookController::class, 'show']);
-Route::post('/books', [BookController::class, 'store']);
-Route::put('/books/{id}', [BookController::class, 'update']);
-Route::delete('/books/{id}', [BookController::class, 'destroy']);
+Route::apiResource('books', BookController::class);
